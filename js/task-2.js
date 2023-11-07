8d34b27b64fde3dff24773ea90dbf2cb1f5d3f30
Pervushyn <142146534+Pervushyn@users.noreply.github.com>
2023-11-07 18:53:38 +0100

    add task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,10 @@
 'use strict';
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return `${message.slice(0, maxLength)}...`;
+function makeArray(firstArray, secondArray, maxLength) {
+  let newArray = firstArray.concat(secondArray);
+  
+  if (newArray.length > maxLength) {
+    newArray = newArray.slice(0, maxLength);
   }
+  
+  return newArray;
 }
-console.log(formatMessage('Curabitur ligula sapien', 16));
-console.log(formatMessage('Curabitur ligula sapien', 23));
-console.log(formatMessage('Vestibulum facilisis purus nec', 20));
-console.log(formatMessage('Vestibulum facilisis purus nec', 30));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
